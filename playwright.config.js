e557411e47adab17876fd798f562e731f3c29214
DESKTOP-1G8BHBB\ASUS <nikunj.trivedi@qable.io>
2025-01-30 11:18:54 +0530

    Chnage in config file

--- playwright.config.js
+++ playwright.config.js
@@ -21,9 +21,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 5,
+  //workers: 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
@@ -33,6 +33,9 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  workers: 4, 
 
   /* Configure projects for major browsers */
   projects: [
