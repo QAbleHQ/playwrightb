5c0ec53508e514c72d1cadd97b7a62f855cbf69e
DESKTOP-1G8BHBB\ASUS <nikunj.trivedi@qable.io>
2025-01-30 13:58:11 +0530

    add more test cases

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
   
   //reporter: [['html', { outputFolder: 'playwright-report' }]],
   reporter: process.env.CI ? 'blob' : 'html',
-  workers: 10, 
+  workers: 11, 
 
   /* Configure projects for major browsers */
   projects: [
